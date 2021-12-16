dd46c671763640f6dc1d34c10584a4d9c66c0acb
Eleanor Bo <yb2503@columbia.edu>
2021-12-15 22:39:50 -0500

    update

--- weather_info_yb2503/tests/test_weather_info_yb2503.py
+++ weather_info_yb2503/tests/test_weather_info_yb2503.py
@@ -1,1 +1,9 @@
 from weather_info_yb2503 import weather_info_yb2503
+import pytest
+
+def test_weather_sector(zipcode, column): 
+    zipcode = 11101
+    column = 'humidity'
+    expected = 74.0
+    actual = weather_sector(zipcode, column)
+    assert actual == expected, 'not works for the zipcode'
